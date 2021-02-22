44757a8d3b29709c0fbe776131ea9b996a9ebbb9
Massimo <massimo.rosas279@gmail.com>
2021-02-22 13:39:22 +0100

    App CSS (mobile + desktop)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,8 +4,10 @@ import './header.scss';
 
 const Header = () => (
   <header className="header">
-    <h1 className="title">Social Media Dashboard</h1>
-    <h3 className="total-followers">Total Followers: 23,004</h3>
+    <div className="header-title">
+      <h1 className="title">Social Media Dashboard</h1>
+      <h3 className="total-followers">Total Followers: 23,004</h3>
+    </div>
     <div className="color-mode">
       <h3 className="color-mode-text">Dark Mode</h3>
       <label className="switch">
